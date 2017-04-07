d44050931986758cf33139b3af7f408f8f57f559
mikeae21 <mae96@txstate.edu>
2017-04-06 23:57:38 -0500

    changed somethings, can upload an image now the image is too big. will have to fix in the next sprint

--- web-app/src/component/imageManager.js
+++ web-app/src/component/imageManager.js
@@ -2,42 +2,78 @@ import lake from './lake.jpg';
 
 var React = require('react');
 var Dropzone = require('react-dropzone');
+var request = require('superagent')
 
 
 
+
+const CLOUDINARY_UPLOAD_PRESET = 'iieovyvu';
+const CLOUDINARY_UPLOAD_URL ='https://api.cloudinary.com/v1_1/mikeycloud9/upload';
 
 class ImageManager extends React.Component {
     constructor(props){
         super(props)
+
+this.state = {
+    uploadedFileCloudinaryUrl: ''
+};
     }
 
-    componentDidMount() {
-        //let canvas = ReactDOM.findDOMNode(this.refs.myCanvas);
-        //let ctx = canvas.getContext('2d');
 
-        //ctx.fillStyle = 'rgb(200,0,0)';
-        //ctx.fillRect(10, 10, 55, 50);
-    }
+onImageDrop(files) {
+    this.setState({
+      uploadedFile: files[0]
+    });
 
-  	upload(){
-  		window.alert("Eventually you can upload an image when you click me.")
-  	}
+    this.handleImageUpload(files[0]);
+  }
+
+  handleImageUpload(file) {
+    let upload = request.post(CLOUDINARY_UPLOAD_URL)
+                     .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+                     .field('file', file);
+
+    upload.end((err, response) => {
+      if (err) {
+        console.error(err);
+      }
+
+      if (response.body.secure_url !== '') {
+        this.setState({
+          uploadedFileCloudinaryUrl: response.body.secure_url
+        });
+      }
+    });
+  }
+
+
+
+
+
 
     render(){
         return (
+            <div>
             <Dropzone
-                multiple={false}
-                accept="image/*"
-                onDrop={this.onImageDrop.bind(this)}>
-                <p>Drop an image or clock to select a file to uploaf.</p>
-                </Dropzone>
-                
+      multiple={false}
+      accept="image/*"
+      onDrop={this.onImageDrop.bind(this)}>
+      <p>Drop an image or click to select a file to upload.</p>
+    </Dropzone>
+    
+      <div className="FileUpload">
+        ...
+      </div>
 
+      <div>
+        {this.state.uploadedFileCloudinaryUrl === '' ? null :
+        <div>
+          <p>{this.state.uploadedFile.name}</p>
+          <img src={this.state.uploadedFileCloudinaryUrl} />
+        </div>}
+      </div>
+    </div>
 
-            <div id='imageManager'>
-                <img src={lake} width={500} height={400} mode='fit'/>
-                <button onClick={this.upload.bind(this)} >Upload new image.</button>
-            </div>
         );
     }
 }
