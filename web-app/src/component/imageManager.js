7407a977050cd8bada1ddb9c69817c61811eda74
Safturento <safturento@gmail.com>
2017-04-13 16:02:35 -0500

    removed dependency on remote server for image upload

--- web-app/src/component/imageManager.js
+++ web-app/src/component/imageManager.js
@@ -4,63 +4,37 @@ var React = require('react');
 var Dropzone = require('react-dropzone');
 var request = require('superagent')
 
-const CLOUDINARY_UPLOAD_PRESET = 'iieovyvu';
-const CLOUDINARY_UPLOAD_URL ='https://api.cloudinary.com/v1_1/mikeycloud9/upload';
-
 class ImageManager extends React.Component {
-		constructor(props){
-				super(props)
-
-			this.state = {
-					uploadedFileCloudinaryUrl: ''
-			};
-		}
-
-
-onImageDrop(files) {
-		this.setState({
-			uploadedFile: files[0]
-		});
-
-		this.handleImageUpload(files[0]);
+	constructor(props){
+			super(props)	
+			this.state ={ uploadedFile: ''}
 	}
 
-	handleImageUpload(file) {
-		let upload = request.post(CLOUDINARY_UPLOAD_URL)
-												.field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
-												.field('file', file);
-
-		upload.end((err, response) => {
-			if (err) {
-				console.error(err);
-			}
-
-			if (response.body.secure_url !== '') {
-				this.setState({
-					uploadedFileCloudinaryUrl: response.body.secure_url
-				});
-			}
-		});
+	getInitialState(){
+		return { uploadedFile: '' }
 	}
 
-		render(){
-			
-			if (this.state.uploadedFileCloudinaryUrl === '')
-				return (
-					<Dropzone
-						multiple={false}
-						accept="image/*"
-						onDrop={this.onImageDrop.bind(this)}>
-						<p>Drop an image or click to select a file to upload.</p>
-					</Dropzone>
-					);
-			else
-				return (
-					<div id="imageManager">
-						<img src={ this.state.uploadedFileCloudinaryUrl} />
-					</div>
+	onImageDrop(files) {
+		this.setState({ uploadedFile: URL.createObjectURL(files[0])});
+	}
+
+	render(){
+		if (this.state.uploadedFile === '')
+			return (
+				<Dropzone
+					multiple={false}
+					accept="image/*"
+					onDrop={this.onImageDrop.bind(this)}>
+					<p>Drop an image or click to select a file to upload.</p>
+				</Dropzone>
 				);
-		}
+		else
+			return (
+				<div id="imageManager">
+					<img src={ this.state.uploadedFile} />
+				</div>
+			);
+	}
 }
 
 module.exports = ImageManager;
