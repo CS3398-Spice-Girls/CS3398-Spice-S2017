82d9b8a8bd2954303e098c2e33b92b5641a20734
Safturento <safturento@gmail.com>
2017-04-13 23:54:02 -0500

    Styled header and sharing button flyout

--- web-app/src/component/sharingButton.js
+++ web-app/src/component/sharingButton.js
@@ -6,14 +6,35 @@ class SharingButton extends React.Component{
 		super(props);
 	}
 
-	copy(){
+	copyHex(){
 		window.prompt("Copy to clipboard", Object.values(this.props.app.palette).join(', '))
+	}
+
+	copyRGB(){
+		var RGBValues = []
+		Object.values(this.props.app.palette).forEach((hex) => {
+			RGBValues.push(this.hexToRGB(hex))
+		})
+		console.log(RGBValues);
+		window.prompt("Copy to clipboard", RGBValues.join(', '))
+	}
+
+	hexToRGB(hex){
+		hex = hex.replace('#','');
+		var r = parseInt(hex.substring(0, hex.length/3), 16);
+		var g = parseInt(hex.substring(hex.length/3, 2*hex.length/3), 16);
+		var b = parseInt(hex.substring(2*hex.length/3, 3*hex.length/3), 16);
+
+		return 'rgba(' + r + ',' + g + ',' + b + ')';
 	}
 
 	render(){
 		return(
 			<div id="share-flyout">
-				<button id="share-button" onClick={this.copy.bind(this)} >Share</button>
+				<button className="flyout-button" id="copy-rgb" onClick={this.copyRGB.bind(this)}>RGB</button>
+				<button className="flyout-button" id="copy-hex" onClick={this.copyHex.bind(this)}>Hex</button>
+				<button className="flyout-button" id="copy-img">Image</button>
+				<button id="share-button">Share</button>
 			</div>
 		);
 	}
